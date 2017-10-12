1433444558362e0c5bbf4bfa9b55ffeaaf9f4200
zhangyuanxing <zhangyuanxing@xiaomi.com>
2017-10-12 10:50:06 +0800

    add shell script

---
+++ zyxvim.sh
@@ -0,0 +1,26 @@
+if [ -e ~/.vimrc ]
+then
+	echo "backup ~/.vimrc as ~/.vimrcbak"
+	mv ~/.vimrc ~/.vimrcbak
+fi
+
+echo "copy .vimrc"
+cp .vimrc ~/.vimrc
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
+echo "clone Vundle and YouCompleteMe"
+git clone https://github.com/VundleVim/Vundle.vim.git
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+echo "install cmake if neccessary"
+sudo apt-get install build-essential cmake
+echo "install python if neccessary"
+sudo apt-get install python-dev python3-dev
+git submodule update --init --recursive
+./install.py
+echo "vim PluginInstall..."
+vim +PluginInstall +qall
+echo "install ctags"
+sudo apt-get install exuberant-ctags
+echo "install astyle"
+sudo apt-get install astyle
